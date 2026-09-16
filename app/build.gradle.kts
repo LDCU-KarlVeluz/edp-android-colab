8be4356f3d754f89eabe8fd31159a8a74de4d068
Ritchie Karl Veluz <rkveluz68722@liceo.edu.ph>
2026-09-16 10:09:16 +0800

    Complete Lab Activity 10: LiceoChat with Retrofit and Offline Caching

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.ksp)
 }
 
 android {
@@ -41,11 +43,23 @@ dependencies {
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.activity.compose)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.material.icons.extended)
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.serialization)
+    implementation(libs.serialization.json)
+    implementation(libs.okhttp.logging)
+
+    implementation(libs.androidx.room.runtime)
+    implementation(libs.androidx.room.ktx)
+    ksp(libs.androidx.room.compiler)
+
     testImplementation(libs.junit)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
--- app/src/main/java/com/example/myapplication/ui/theme/Color.kt
+++ app/src/main/java/com/example/myapplication/ui/theme/Color.kt
@@ -2,6 +2,9 @@ package com.example.myapplication.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+val MaroonPrimary = Color(0xFF800000)
+val GoldTertiary = Color(0xFFFFD700)
+
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
